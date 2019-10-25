8e93b58efdfc191febefab6a7773d69a1f5af678
Thomas Ruggeri <truggeri@gmail.com>
2019-10-25 10:33:47 -0400

    Using volume in default docker pg

--- lib/tasks/docker_database.rb
+++ lib/tasks/docker_database.rb
@@ -3,9 +3,19 @@ namespace :docker do
     desc "Starts a dev database using docker"
     task :start do
       database_port = ENV["MEAL_PLANNER_API_DB_PORT"].to_i.presence || 15_400
-      # rubocop:disable Metrics/LineLength
-      `docker run --name meal_planner_api_db --detach --publish #{database_port}:5432 -e POSTGRES_DB=$MEAL_PLANNER_API_DB_DATABASE -e POSTGRES_USER=$MEAL_PLANNER_API_DB_USERNAME -e POSTGRES_PASSWORD=$MEAL_PLANNER_API_DB_PASSWORD postgres:11.4`
-      # rubocop:enable Metrics/LineLength
+      work_dir = `pwd`.chomp
+      `mkdir -p #{work_dir}/data/db`
+
+      docker_cmd_options = []
+      docker_cmd_options << "--name meal_planner_api_db"
+      docker_cmd_options << "--detach"
+      docker_cmd_options << "--publish #{database_port}:5432"
+      docker_cmd_options << "--volume #{work_dir}/data/db:/var/lib/postgresql/data"
+      docker_cmd_options << "--env POSTGRES_DB=$MEAL_PLANNER_API_DB_DATABASE"
+      docker_cmd_options << "--env POSTGRES_USER=$MEAL_PLANNER_API_DB_USERNAME"
+      docker_cmd_options << "--env POSTGRES_PASSWORD=$MEAL_PLANNER_API_DB_PASSWORD"
+
+      system("docker run #{docker_cmd_options.join(' ')} postgres:11.5")
     end
 
     desc "Stops a dev database using docker"
